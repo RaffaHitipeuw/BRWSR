239d4124ee9c4ac994fb7041880c8a6fc3f80f85
raffa <raffahitipeuw@gmail.com>
2026-09-20 14:47:18 +0700

    fix: setup background update

--- apps/browser/src-tauri/src/main.rs
+++ apps/browser/src-tauri/src/main.rs
@@ -5699,6 +5699,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(Mutex::new(MemoryTracker::default()))
         .manage(Mutex::new(SequenceTracker::default()))
         .manage(WebViewLifecycle::default())
